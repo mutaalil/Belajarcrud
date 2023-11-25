8ca11af0f4f0436db62cb9f65275069a24fdd1a4
mutaalil <mutaalilmatin@gmail.com>
2023-11-25 14:15:59 +0700

    CRUD

---
+++ datasekolah/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTV-8">
+        <meta http-equiv="X-UA-Compaitible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <h1>Sistem Informasi Sekolah</h1>
+        <h2>SMK Unipdu</h2>
+        <a href="data_siswa.php">Daftar Siswa</a><br>
+        <a href="tambah_siswa.php">Pendaftaran</a>
+</body>
+</html>
